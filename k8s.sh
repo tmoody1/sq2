0b264f8444466a393e99cc630551850212df63a7
Thomas Moody <Thomas.moody@imanage.com>
2019-12-11 10:54:51 +0000

    travis failing

--- k8s.sh
+++ k8s.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+ls
+whoami
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-
 mkdir ${HOME}/.kube
 envsubst <config > ${HOME}/.kube/config
